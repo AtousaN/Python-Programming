d8c6663629c42b3134777ff76c4a0c7df8342f69
AtousaN <68468228+AtousaN@users.noreply.github.com>
2020-07-22 18:07:00 +0430

    Add files via upload

---
+++ Leap Year 1.py
@@ -0,0 +1,10 @@
+year = int(input('Enter year number: '))
+if year%4 != 0 :
+    print('not Leap Year')
+elif year%4 == 0 and year%100 == 0 and year%400 == 0 :
+    print('Leap Year')
+elif year%4 == 0 and year%100 == 0 :
+    print('not Leap Year')
+else:
+     print('Leap Year')
+     
